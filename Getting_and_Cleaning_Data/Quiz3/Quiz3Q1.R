22dea24ecc31e1ba34d8b20b19045781155a9e87
Prabath Peiris <peiris.prabath@gmail.com>
2015-08-08 18:57:05 -0400

    Update the Quiz 3

--- Getting_and_Cleaning_Data/Quiz3/Quiz3Q1.R
+++ Getting_and_Cleaning_Data/Quiz3/Quiz3Q1.R
@@ -2,9 +2,6 @@ if (!file.exists("Fss06hid.csv")) {
     download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv', destfile = "Fss06hid.csv", method="curl")
 }
 
-data <- read.csv("Fss06hid.csv")
+agricultureLogical <- read.csv("Fss06hid.csv")
 
-
-
-logicalVec <- data$
-
+print(which(agricultureLogical$ACR == 3 & agricultureLogical$AGS == 6))
--- Getting_and_Cleaning_Data/Quiz3/Quiz3Q3.R
+++ Getting_and_Cleaning_Data/Quiz3/Quiz3Q3.R
@@ -2,15 +2,11 @@
 
 gdp = read.csv("getdata-data-GDP.csv", skip = 4)
 gdp = gdp[1:190, ] # load the 190 cuntries
+gdp_code = data.frame("CountryCode" = as.character(gdp[, 1]), "gdp" = ((gdp[, 5])))
+# print(gdp_code)
+# # gdp_code = order(gdp_code, na.last = TRUE, decreasing = FALSE)
 
-
-
-gdp_code = data.frame("CountryCode" = as.character(gdp[, 1]), "gdp" = (gdp[, 5]))
-
-
-# gdp_code = order(gdp_code, na.last = TRUE, decreasing = FALSE)
-
-# print(head(gdp_code))
+# # print(head(gdp_code))
 
 
 
@@ -20,14 +16,36 @@ ed = read.csv("getdata-data-EDSTATS_Country.csv")
 ed_code = data.frame("X" = ed$CountryCode)
 
 
+
 mdata = merge(gdp_code, ed_code, by.x = "CountryCode", by.y = "X")
 
-mdata = order(mdata, decreasing = TRUE)
+
+print(mdata)
 
 
-print(head(values))
+# mdata = order(mdata, decreasing = TRUE)
 
-print(head(mdata$gdp))
+# print(mdata)
+
+# print(mdata[order(mdata[,"gdp"]), ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(str(mdata))
+
+# print(head(values))
+
+# print(head(mdata$gdp))
 
 # print(unique(mdata))
 
